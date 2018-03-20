b0add4dc8343620ee13f98ec0b78538ec96379a5
ghildreth <gabrielantonhildreth@gmail.com>
2018-03-20 02:35:46 +0000

    added a forEach loop

--- download_avatar.js
+++ download_avatar.js
@@ -1,10 +1,10 @@
 var request = require('request');
 var secrets = require('./secrets.js');
-
 function getRepoContributors(repoOwner, repoName, cb) {
-  // console.log('Welcome to the GitHub Avatar Downloader!');
+  console.log('Welcome to the GitHub Avatar Downloader!');
   var options = {
     url: "https://api.github.com/repos/" + repoOwner + "/" + repoName + "/contributors",
+    json: true,
     headers: {
       'User-Agent': 'request',
       'Auth': secrets.GITHUB_TOKEN
@@ -14,11 +14,15 @@ function getRepoContributors(repoOwner, repoName, cb) {
 
   request(options, function(err, res, body) {
     cb(err, body);
+    body.forEach( function (object) {
+      console.log(object['avatar_url']);
+
+    });
   });
+
 }
 
-
-getRepoContributors ("jquery", "jquery", function(err, result) {
+getRepoContributors("jquery", "jquery", function(err, result) {
   console.log("Errors:", err);
   console.log("Results:", result);
 });
