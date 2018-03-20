5bc7e2dd8738d4302513d0e6d80c8680410d761b
ghildreth <gabrielantonhildreth@gmail.com>
2018-03-20 01:55:09 +0000

    tried to add secret.js info without commiting

--- download_avatar.js
+++ download_avatar.js
@@ -1,12 +1,24 @@
 var request = require('request');
+var secrets = require('./secrets.js');
 
-function getRepoContributors(RepoOwner, repoName, cb) {
+function getRepoContributors(repoOwner, repoName, cb) {
+  // console.log('Welcome to the GitHub Avatar Downloader!');
+  var options = {
+    url: "https://api.github.com/repos/" + repoOwner + "/" + repoName + "/contributors",
+    headers: {
+      'User-Agent': 'request',
+      'Auth': secrets.GITHUB_TOKEN
+    }
+  };
 
+
+  request(options, function(err, res, body) {
+    cb(err, body);
+  });
 }
 
-console.log('Welcome to the GitHub Avatar Downloader!');
 
-getRepoContributors("jquery", "jquery", function(err, result) {
+getRepoContributors ("jquery", "jquery", function(err, result) {
   console.log("Errors:", err);
   console.log("Results:", result);
 });
