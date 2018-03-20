03a99a44f84c42b27d8f0a62c903bd0d34cb2e59
ghildreth <gabrielantonhildreth@gmail.com>
2018-03-20 03:37:23 +0000

    added filepath and url in order to DL images"

--- download_avatar.js
+++ download_avatar.js
@@ -12,17 +12,33 @@ function getRepoContributors(repoOwner, repoName, cb) {
   };
 
 
-  request(options, function(err, res, body) {
-    cb(err, body);
-    body.forEach( function (object) {
-      console.log(object['avatar_url']);
+  // request(options, function(err, res, body) {
+  //   cb(err, body);
+  //   body.forEach( function (object) {
+  //     console.log(object['avatar_url']);
 
-    });
-  });
+  //   });
+  // });
 
 }
+
+
 
 getRepoContributors("jquery", "jquery", function(err, result) {
   console.log("Errors:", err);
   console.log("Results:", result);
 });
+
+var fs = require('fs');
+function downloadImageByURL(url, filePath) {
+  // filePath = './future.jpg';
+  request.get(url)
+  .on('error', function (err) {
+
+    throw err;
+
+  })
+  .pipe(fs.createWriteStream(filePath));
+}
+
+downloadImageByURL("https://avatars2.githubusercontent.com/u/2741?v=3&s=466", 'avatars/kvirani.jpg');
